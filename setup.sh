11ad32d5de36d1bce08a14b33f77e3c1d178b6a5
Huy Tran <h255t794@altra.ittc.ku.edu>
2024-01-29 19:54:33 -0600

    udpate

--- setup.sh
+++ setup.sh
@@ -11,6 +11,13 @@ export SPARK_HOME=/opt/spark-3.3.2
 export SPARK_MASTER_PORT=7077
 export SPARK_WEBUI_PORT=8080
 
+# Download dataset
+mkdir worker/data
+wget --no-check-certificate -O ./worker/data/Twitter-dataset.zip http://datasets.epfl.ch/cloudsuite/twitter_dataset.zip
+unzip -d ./worker/data ./worker/data/Twitter-dataset.zip
+rm ./worker/data/Twitter-dataset.zip
+
+# Uncomment this to Install Spark and Scala
 # wget --progress=bar:force https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}${SCALA_VERSION}.tgz
 # mkdir -p $SPARK_HOME
 # tar -xzf spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}${SCALA_VERSION}.tgz --directory=$SPARK_HOME --strip 1
